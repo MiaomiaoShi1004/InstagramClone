4e7cb3dcd56b6062e5a44f032def04ee99714c76
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-08-09 21:31:40 +0200

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Miaomiao Shi on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
