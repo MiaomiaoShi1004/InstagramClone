691a3ab6d56e102804c1c362a6f415efeb05bb55
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-08-23 23:35:53 +0200

    Page UI

---
+++ InstagramClone/Core/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  InstagramClone
+//
+//  Created by Miaomiao Shi on 10/08/2024.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
