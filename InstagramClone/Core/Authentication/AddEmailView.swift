f21abe7fc5eaea44b35211827c7841d6aa2b7fb7
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-08-24 17:30:57 +0200

    AddEmailView

---
+++ InstagramClone/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEmailView.swift
+//  InstagramClone
+//
+//  Created by Miaomiao Shi on 24/08/2024.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEmailView()
+}
