9eb2faa2f63b23e2d5a9a7aa3e64ab20e30ed247
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-08-24 23:59:56 +0200

    complete sign up view

---
+++ InstagramClone/Core/Authentication/CreatePasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePasswordView.swift
+//  InstagramClone
+//
+//  Created by Miaomiao Shi on 24/08/2024.
+//
+
+import SwiftUI
+
+struct CreatePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatePasswordView()
+}
