b52f8f334a2f1d0d06793435150df9e2a19fa427
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-08-24 17:08:35 +0200

    Login UI

---
+++ InstagramClone/Core/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  InstagramClone
+//
+//  Created by Miaomiao Shi on 23/08/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
