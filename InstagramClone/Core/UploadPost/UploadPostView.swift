11b6c34da8ccea82949d7de7e69950077c4aab63
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-08-26 23:06:14 +0200

    UploadPostView

---
+++ InstagramClone/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadPostView.swift
+//  InstagramClone
+//
+//  Created by Miaomiao Shi on 26/08/2024.
+//
+
+import SwiftUI
+
+struct UploadPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadPostView()
+}
